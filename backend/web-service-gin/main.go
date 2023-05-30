17565b6106f6d4ba5ca32105c5f534749f0c5442
lucilaGomez <luligomez.ar@gmail.com>
2023-05-30 17:08:45 -0300

    main y api

---
+++ backend/web-service-gin/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+//estructura hotel
+
+type hotel struct {
+	ID          string  `json:"id"`
+	Name        string  `json: name`
+	Description string  `json: description`
+	Price       float64 `json: price`
+}
+
+//estructura reservas
+
+type reservation struct {
+	ID          string `json:"id"`
+	IsConfirmed bool   `json:"isConfirmed"`
+	IdHotel     string `json:"idHotel"`
+	From        string `json:"from"`
+	To          string `json:"to"`
+}
+
+//confirma la reserva
+
+func confirmReservation(reservation *reservation) {
+
+	reservation.IsConfirmed = true
+}
+
+//crea los hoteles
+
+var hotels = []hotel{
+	{ID: "1", Name: "nombre 1", Description: "descr.", Price: 120.50},
+	{ID: "2", Name: "Hotel Baraton", Description: "Un hotel sucio pero barato.", Price: 21.30},
+	{ID: "3", Name: "La Posada de Roberto", Description: "Descanso campestre", Price: 89.50},
+}
+
+//crea resevas
+
+var reservations = []reservation{
+
+	{ID: "1", IsConfirmed: false, IdHotel: "1", From: "2023-05-23", To: "2023-06-03"},
+	{ID: "2", IsConfirmed: false, IdHotel: "2", From: "2024-05-23", To: "2025-06-03"},
+	{ID: "3", IsConfirmed: false, IdHotel: "3", From: "2020-05-23", To: "2020-06-03"},
+}
+
+/*
+	GET --- listar hoteles
+	agregar un hotel
+	ver detalles de un hotel
+
+	GET --- listar reservas
+	confirmar una reserva ----------- FALTA VALIDACION
+
+*/
+
+func main() {
+
+	router := gin.Default()
+	router.GET("/hotels", getHotels)
+	router.GET("/hotels/:id", getHotelById)
+	router.POST("/hotels", postHotels)
+	router.GET("/reservations", getReservations)
+	router.GET("/reservations/:id", getReservationById)
+	router.POST("/reservations", postReservations)
+
+	router.Run("localhost:8080")
+
+}
+
+func getHotels(context *gin.Context) {
+	context.IndentedJSON(http.StatusOK, hotels)
+}
+
+func postHotels(context *gin.Context) {
+	var newHotel hotel
+	if err := context.BindJSON(&newHotel); err != nil {
+		return
+	}
+
+	hotels = append(hotels, newHotel)
+	context.IndentedJSON(http.StatusCreated, newHotel)
+}
+
+func getHotelById(context *gin.Context) {
+	id := context.Param("id")
+
+	for _, hotel := range hotels {
+		if hotel.ID == id {
+			context.IndentedJSON(http.StatusOK, hotel)
+			return
+		}
+	}
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "hotel no encontrado"})
+}
+
+func getReservations(context *gin.Context) {
+
+	context.IndentedJSON(http.StatusOK, reservations)
+}
+
+func getReservationById(context *gin.Context) {
+	id := context.Param("id")
+
+	for _, reservation := range reservations {
+		if reservation.ID == id {
+			context.IndentedJSON(http.StatusOK, reservation)
+			return
+		}
+	}
+	context.IndentedJSON(http.StatusNotFound, gin.H{"message": "reserva no encontrada"})
+}
+
+func postReservations(context *gin.Context) {
+	var newReservation reservation
+	if err := context.BindJSON(&newReservation); err != nil {
+		return
+	}
+
+	reservations = append(reservations, newReservation)
+	context.IndentedJSON(http.StatusCreated, newReservation)
+}
