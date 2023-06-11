b9d339cbc3efe1cd31b4846de22a8c3b12961774
francotalloneucc <2109774@ucc.edu.ar>
2023-06-11 18:25:57 -0300

    cambios pequeños

--- frontend/client/src/components/Home.jsx
+++ frontend/client/src/components/Home.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 export function Home({ user }){
     return (
         <section>
-            <h1 className="bienvenido">Bienvenido</h1>
+            <h1 className="bienvenido">Bienvenid@</h1>
             <h2 className="username">{user}</h2>
         </section>
     )
